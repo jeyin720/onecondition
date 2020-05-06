08cc92ab68897202b5c18bad4b824919e3f206d0
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-06 20:10:39 +0900

    초기 커밋.

---
+++ onecondition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onecondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if(x>10&&x<20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
+        }
+    }
+}
